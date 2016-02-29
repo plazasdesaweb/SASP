db1968611131a3fd0d0471338f2a93a4ca3f34be
Nelson Arturo Rodriguez Briceño <nelson.a.rodriguez@gmail.com>
2016-02-29 17:58:30 -0430

    V1.0.6
    -Corrección en manejo de trackii y creation_date en tabla tajetas.
    -Corrección para retornar trackii en busqueda por id de afiliado.
    -Correción en la reimpresión de tarjetas, para no simular la impresión de la tarjeta actual.

--- Suma2Lealtad/Controllers/Prepago/BeneficiarioPrepagoController.cs
+++ Suma2Lealtad/Controllers/Prepago/BeneficiarioPrepagoController.cs
@@ -243,36 +243,16 @@ namespace Suma2Lealtad.Controllers.Prepago
         public ActionResult ImprimirTarjeta(int id)
         {
             BeneficiarioPrepago beneficiario = repBeneficiario.Find(id);
-            beneficiario.Afiliado.trackI = Tarjeta.ConstruirTrackI(beneficiario.Afiliado.pan);
-            beneficiario.Afiliado.trackII = Tarjeta.ConstruirTrackII(beneficiario.Afiliado.pan);
             return View("ImpresoraImprimirTarjeta", beneficiario);
         }
 
         public ActionResult ReImprimirTarjeta(int id, int idCliente)
         {
             ViewModel viewmodel = new ViewModel();
-            //BeneficiarioPrepago beneficiario = new BeneficiarioPrepago()
-            //{
-            //    Afiliado = repAfiliado.Find(id),
-            //    Cliente = repCliente.Find(idCliente)
-            //};
             BeneficiarioPrepago beneficiario = repBeneficiario.Find(id);
-            if (repAfiliado.ImprimirTarjeta(beneficiario.Afiliado))
+            if (repAfiliado.BloquearTarjeta(beneficiario.Afiliado))
             {
-                if (repAfiliado.BloquearTarjeta(beneficiario.Afiliado))
-                {
-                    beneficiario.Afiliado.trackI = Tarjeta.ConstruirTrackI(beneficiario.Afiliado.pan);
-                    beneficiario.Afiliado.trackII = Tarjeta.ConstruirTrackII(beneficiario.Afiliado.pan);
-                    return View("ImpresoraImprimirTarjeta", beneficiario);
-                }
-                else
-                {
-                    viewmodel.Title = "Prepago / Beneficiario / ReImprimir Tarjeta";
-                    viewmodel.Message = "Falló el proceso de reimpresión de la Tarjeta";
-                    viewmodel.ControllerName = "BeneficiarioPrepago";
-                    viewmodel.ActionName = "FilterReview";
-                    return RedirectToAction("GenericView", viewmodel);
-                }
+                return View("ImpresoraImprimirTarjeta", beneficiario);
             }
             else
             {
@@ -289,8 +269,6 @@ namespace Suma2Lealtad.Controllers.Prepago
         {
             ViewModel viewmodel = new ViewModel();
             BeneficiarioPrepago beneficiario = repBeneficiario.Find(id);
-            //beneficiario.Afiliado.trackI = Tarjeta.ConstruirTrackI(beneficiario.Afiliado.pan);
-            //beneficiario.Afiliado.trackII = Tarjeta.ConstruirTrackII(beneficiario.Afiliado.pan);
             if (repAfiliado.ImprimirTarjeta(beneficiario.Afiliado))
             {
                 viewmodel.Title = "Prepago / Beneficiario / Operaciones con la Impresora / Imprimir Tarjeta";
